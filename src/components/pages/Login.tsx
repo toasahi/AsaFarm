2bb4f300eeaea4b3004ca9e92ac4e7cbc2f7414a
toasahi <asa1313to@gmail.com>
2021-05-29 09:23:17 +0900

    Add Button && Complete half Page

--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -4,11 +4,15 @@ export const Login: VFC = memo(() => {
   return (
     <>
       <div className="lg:flex">
-        <div>def</div>
         <div className="lg:w-1/2 xl:max-w-screen-sm">
-          <div></div>
-          <div className="mt-10 px-12 sm:px-24 md:px-48 lg:px-12 lg:mt-16 xl:mt-24 xl:max-w-screen-2xl">
-            <h2 className="text-center text-4xl font-semibold lg:text-left xl:text-5xl xl:font-bold">Log In</h2>
+          <div className="py-12 bg-green-100 lg:bg-white flex justify-center lg:justify-start lg:px-12">
+            <div></div>
+            <div className="text-2xl text-green-700 tracking-wide ml-2 font-semibold">AsaFarm</div>
+          </div>
+          <div className="mt-10 px-12 sm:px-24 md:px-48 lg:px-12 lg:mt-16 xl:mt-16 xl:max-w-screen-2xl">
+            <h2 className="text-center text-4xl font-semibold text-green-800 lg:text-left xl:text-5xl xl:font-bold">
+              Log In
+            </h2>
             <div className="mt-12">
               <form>
                 <div>
@@ -31,13 +35,23 @@ export const Login: VFC = memo(() => {
                     placeholder="Enter Your Password"
                   />
                 </div>
-                <div>
-                  <button />
+                <div className="mt-10">
+                  <button
+                    className="bg-green-500 text-gray-100 p-4 w-full rounded-full tracking-wide
+                  font-semibold focus:outline-none focus:shadow-outline hover:bg-green-600"
+                  >
+                    Log In
+                  </button>
                 </div>
               </form>
+              <div className="mt-12 text-base font-semibold text-gray-500 text-center">
+                Don't have an account?
+                <a className="cursor-pointer text-green-600 hover:text-green-800">Sign Up</a>
+              </div>
             </div>
           </div>
         </div>
+        <div className="hidden lg:flex items-center bg-green-50 flex-1 h-screen"></div>
       </div>
     </>
   );
